1e6cea703de38d6ed6dc57f77c67b3f061095807
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-11-28 18:26:39 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import $ from 'jquery';
+import Poster from './Poster';
+
+class App extends Component {
+	constructor(props){
+		// App is a subclass of Component. Therefore, we MUST include a super.
+		super(props);
+		this.state = {
+			movies: []
+		}
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	// I am special. I will run ONE time, before the first render.
+	componentWillMount(){
+		console.log("The component is about to mount.");
+	}
+
+	// I am special. I will run ONE time, after the first render.
+	componentDidMount(){
+		console.log("The component mounted.");
+		var url = 'http://api.themoviedb.org/3/movie/now_playing?api_key=fec8b5ab27b292a68294261bb21b04a5';
+		$.getJSON(url, (movieData)=>{
+			console.log(movieData);
+			this.setState({
+				movies: movieData.results
+			});
+		});
+	}
+
+	handleSubmit(event){
+		event.preventDefault();
+		var value = document.getElementById('searchTerm').value; // Ok to use jQuery, just DON'T mess with the DOM!
+		document.getElementById('searchTerm').value = "";
+		console.log("Form submitted!");
+		// console.log(value);
+		var url = `https://api.themoviedb.org/3/search/movie?api_key=fec8b5ab27b292a68294261bb21b04a5&query=${value}`;
+		// console.log(url);
+		$.getJSON(url, (movieSearchData)=>{
+			// We have the new movies. Update state.
+			this.setState({
+				movies: movieSearchData.results
+			});
+		});
+	}
+
+	render() {
+		var postersArray = [];
+
+		// First time through (when the component mounts), this.state.movies will be an empty array.
+		this.state.movies.map((movie, index)=>{
+			// postersArray.push(<Poster key={index} id={movie.id} overview={movie.overview} title={movie.title} poster={movie.poster_path} />)
+			// When passing a lot of props, you can send the entire movie object.
+			postersArray.push(<Poster key={index} movie={movie} />)
+			return null; // To get the linter to shut up (it expects a return from map)...
+		});
+
+		// postersArray = this.state.movies.map((movie, index)=>{
+		// 	return (<Poster key={index} id={movie.id} overview={movie.overview} title={movie.title} poster={movie.poster_path} />)
+		// });
+
+		return (
+			<div className="App">
+				<h1>This is the movie app...again.</h1>
+				<div className="col-sm-12 text-center">
+					<form onSubmit={this.handleSubmit}>
+						<input id="searchTerm" type="text" placeholder="Movie Title" />
+						<button type="submit" className="btn btn-primary">Search</button>
+					</form>
+				</div>
+				{postersArray}
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/Poster.js
@@ -0,0 +1,19 @@
+// The very first thing in most components is to import React.
+import React, { Component } from 'react';
+
+// I am a presentational component. I could have been in app.js,
+// but it's cleaner if I'm over here.
+class Poster extends Component{
+	render(){
+		var imagePath = `http://image.tmdb.org/t/p/w300${this.props.movie.poster_path}`;
+		var moviePage = `https://www.themoviedb.org/movie/${this.props.movie.id}`;
+		return(
+			<div className='col-sm-3'>
+				<a href={moviePage} target="_blank"><img src={imagePath} alt={this.props.movie.overview}/></a>
+				<h4>{this.props.movie.title}</h4><br />
+			</div>
+		)
+	}
+}
+
+export default Poster;
